2ff8438581fbbf0fafc25351f52d221f83a5abde
Jorge Tamariz <jtamariz@flexiti.com>
2024-02-12 22:47:05 -0400

    Finished the whole project

---
+++ Cafe.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace semaphore
+{
+    public static class Cafe
+    {
+        public readonly static string[] Products = [
+            "Coffee",
+            "Espresso",
+            "Latte",
+            "Cappuccino",
+            "Macchiato",
+            "Mocha",
+            "Americano",
+            "Tea",
+            "Croissant",
+            "Muffin",
+            "Bagel",
+            "Sandwich",
+            "Salad",
+            "Smoothie",
+            "Juice",
+            "Cake",
+            "Cookie",
+            "Scone"
+        ];
+
+    }
+}
